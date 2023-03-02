97799eeae1cff6c55971b0e177c5097a5b752264
Tiago Cunha <tiago92cunha@gmail.com>
2023-03-02 23:48:28 +0000

    Remove upload card on submit

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -29,42 +29,44 @@ function App() {
   };
 
   return (
-    <div className="flex h-full flex-col bg-white px-1 pt-3 text-xs">
-      <div className="grid grid-rows-3 items-center justify-center gap-3 p-3">
-        <Select label="Choose your table configuration:" />
-        <div>
-          <Dropzone
-            onDrop={(acceptedFiles) => setFiles(acceptedFiles)}
-            accept={{ "text/csv": [".csv"] }}
-          >
-            {({ getRootProps, getInputProps }) => (
-              <section className="h-40 w-80 rounded-md border border-dashed border-primary hover:cursor-pointer">
-                <div
-                  {...getRootProps()}
-                  className="flex h-full w-full items-center justify-center bg-indigo-100 text-center"
-                >
-                  <input {...getInputProps()} />
-                  <p className="text-base text-primary">
-                    Drag 'n' drop some files here, or click to select files
-                  </p>
-                </div>
-              </section>
+    <div className="flex h-full flex-col items-center bg-white px-1 pt-3 text-xs">
+      {!render && (
+        <div className="grid w-fit grid-rows-3 items-center gap-3 rounded-lg border p-3 shadow-md">
+          <Select label="Choose your table configuration:" />
+          <div>
+            <Dropzone
+              onDrop={(acceptedFiles) => setFiles(acceptedFiles)}
+              accept={{ "text/csv": [".csv"] }}
+            >
+              {({ getRootProps, getInputProps }) => (
+                <section className="h-40 w-80 rounded-md border border-dashed border-primary hover:cursor-pointer">
+                  <div
+                    {...getRootProps()}
+                    className="flex h-full w-full items-center justify-center bg-indigo-100 text-center"
+                  >
+                    <input {...getInputProps()} />
+                    <p className="text-base text-primary">
+                      Drag 'n' drop some files here, or click to select files
+                    </p>
+                  </div>
+                </section>
+              )}
+            </Dropzone>
+            {files !== undefined && (
+              <Text text={`${files[0].name}.${files[0].type}`} />
             )}
-          </Dropzone>
-          {files !== undefined && (
-            <Text text={`${files[0].name}.${files[0].type}`} />
-          )}
+          </div>
+          <div className="flex justify-center">
+            <Button
+              disabled={files === undefined}
+              intent="primary"
+              onClick={parseCsv}
+            >
+              Generate table
+            </Button>
+          </div>
         </div>
-        <div className="flex justify-center">
-          <Button
-            disabled={files === undefined}
-            intent="primary"
-            onClick={parseCsv}
-          >
-            Generate table
-          </Button>
-        </div>
-      </div>
+      )}
       {render && <BasicTable headers={headers ?? []} data={data} />}
     </div>
   );
