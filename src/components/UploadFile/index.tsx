ca76cf2641f9000f454b47b53b2a9bb7f44ce233
Tiago Cunha <tiago92cunha@gmail.com>
2023-03-04 14:42:29 +0000

    Create Upload component

---
+++ src/components/UploadFile/index.tsx
@@ -0,0 +1,56 @@
+import { Title } from "@/components/ui/Typography/Title";
+import { Text } from "@/components/ui/Typography/Text";
+import Dropzone, { useDropzone } from "react-dropzone";
+import React, { useEffect, useState } from "react";
+import { Select } from "@/components/ui/Select";
+import { Button } from "@/components/ui/Button";
+
+type UploadFileProps = {
+  onFileUpload: (file: File) => void;
+};
+
+export const UploadFile: React.FC<UploadFileProps> = ({ onFileUpload }) => {
+  const [files, setFiles] = useState<File[]>();
+
+  const uploadFile = () => {
+    if (files !== undefined && files?.length > 0) onFileUpload(files[0]);
+  };
+
+  return (
+    <div className="flex w-fit flex-col items-center gap-10 rounded-lg border p-3 shadow-md">
+      <Select label="Choose your table configuration:" />
+      <div>
+        <Dropzone
+          onDrop={(acceptedFiles) => setFiles(acceptedFiles)}
+          accept={{ "text/csv": [".csv"] }}
+        >
+          {({ getRootProps, getInputProps }) => (
+            <section className="h-40 w-80 rounded-md border border-dashed border-primary hover:cursor-pointer">
+              <div
+                {...getRootProps()}
+                className="flex h-full w-full items-center justify-center bg-indigo-100 text-center"
+              >
+                <input {...getInputProps()} />
+                <p className="text-base text-primary">
+                  Drag 'n' drop some files here, or click to select files
+                </p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+        {files !== undefined && (
+          <Text text={`${files[0].name}.${files[0].type}`} />
+        )}
+      </div>
+      <div className="flex justify-center">
+        <Button
+          disabled={files === undefined || files?.length === 0}
+          intent="primary"
+          onClick={() => uploadFile()}
+        >
+          Generate table
+        </Button>
+      </div>
+    </div>
+  );
+};
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,24 +1,19 @@
-import { Title } from "@/components/ui/Typography/Title";
-import { Text } from "@/components/ui/Typography/Text";
-import Dropzone, { useDropzone } from "react-dropzone";
 import React, { useEffect, useState } from "react";
-import { Select } from "@/components/ui/Select";
-import { Button } from "@/components/ui/Button";
 import Papa from "papaparse";
 import { BasicTable } from "@/components/Table/BasicTable";
+import { UploadFile } from "@/components/UploadFile";
 
 type TTableData = {
   [key: string]: string | number;
 };
 
 function App() {
-  const [files, setFiles] = useState<File[]>();
   const [headers, setHeaders] = useState<Array<string>>([]);
   const [data, setData] = useState<Array<TTableData>>([]);
   const [render, setRender] = useState<boolean>(false);
 
-  const parseCsv = () => {
-    Papa.parse(files[0], {
+  const parseCsv = (file: File) => {
+    Papa.parse(file, {
       header: true,
       complete: (results: any) => {
         setHeaders(results.meta.fields);
@@ -30,43 +25,7 @@ function App() {
 
   return (
     <div className="flex h-full flex-col items-center bg-white px-1 pt-3 text-xs">
-      {!render && (
-        <div className="grid w-fit grid-rows-3 items-center gap-3 rounded-lg border p-3 shadow-md">
-          <Select label="Choose your table configuration:" />
-          <div>
-            <Dropzone
-              onDrop={(acceptedFiles) => setFiles(acceptedFiles)}
-              accept={{ "text/csv": [".csv"] }}
-            >
-              {({ getRootProps, getInputProps }) => (
-                <section className="h-40 w-80 rounded-md border border-dashed border-primary hover:cursor-pointer">
-                  <div
-                    {...getRootProps()}
-                    className="flex h-full w-full items-center justify-center bg-indigo-100 text-center"
-                  >
-                    <input {...getInputProps()} />
-                    <p className="text-base text-primary">
-                      Drag 'n' drop some files here, or click to select files
-                    </p>
-                  </div>
-                </section>
-              )}
-            </Dropzone>
-            {files !== undefined && (
-              <Text text={`${files[0].name}.${files[0].type}`} />
-            )}
-          </div>
-          <div className="flex justify-center">
-            <Button
-              disabled={files === undefined}
-              intent="primary"
-              onClick={parseCsv}
-            >
-              Generate table
-            </Button>
-          </div>
-        </div>
-      )}
+      {!render && <UploadFile onFileUpload={parseCsv} />}
       {render && <BasicTable headers={headers ?? []} data={data} />}
     </div>
   );
